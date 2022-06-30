c6b7efdbbd69cbd1155f3ddb681dc5903ec7f596
Antonio <antoniopsilva083@gmail.com>
2022-06-29 23:22:30 -0300

    start project

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import { Banner } from './components/Banner/Banner'
+
+export function App() {
+  return (
+    <Banner />
+  )
+}
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,9 @@
+import bannerImg from '../../assets/banner.png';
+
+export function Banner() {
+  return (
+    <header>
+      <img src={bannerImg} alt="Banner" />
+    </header>
+  )
+}
